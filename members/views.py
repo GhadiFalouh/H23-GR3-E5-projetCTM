43d3d75ad3cdca9e15d239299557287959e4abd9
GhadiFalouh <ghadif2001@hotmail.com>
2023-02-27 00:42:07 -0500

    abc

--- members/views.py
+++ members/views.py
@@ -1,9 +1,12 @@
+import csv
+
+import requests
 from django.shortcuts import render
 from django.template import loader
 from .models import Member
 from django.http import HttpResponse, HttpResponseRedirect
 
-api_key =('Z8O8EWVEWLEFPD4X')
+
 
 def members(request):
     mymembers = Member.objects.all().values()
@@ -38,9 +41,19 @@ def testing(request):
 
 
 def testtest(request):
-    mymembers = Member.objects.all().values()
-    template = loader.get_template('testtest.html')
-    context = {
-        'mymembers': mymembers,
-    }
-    return HttpResponse(template.render(context, request))
+    api_key = ('Z8O8EWVEWLEFPD4X')
+    if request.method == 'POST':
+        nomAction = request.POST.get('nomAction', '')
+        CSV_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=" + nomAction + "&interval=60min&slice=year1month1&apikey=" + api_key
+        with requests.Session() as s:
+            download = s.get(CSV_URL)
+            decoded_content = download.content.decode('utf-8')
+            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+            my_list = list(cr)
+        context = {
+            'my_list': my_list,
+            'nomAction': nomAction
+        }
+    else:
+        context = {}
+    return render(request, 'testtest.html', context)
