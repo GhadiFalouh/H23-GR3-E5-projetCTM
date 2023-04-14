9a63ab786fcea27fea9f5589917fcbc693d5b8e0
GhadiFalouh <ghadif2001@hotmail.com>
2023-04-14 08:43:11 -0400

    OK

--- projetCTM/settings.py
+++ projetCTM/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = "projetCTM.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,7 +93,7 @@ DATABASES = {
     }
 }
 
-'''
+
 
 
 '''
