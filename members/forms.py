3d4adfbda02808c6d5fc8fc275377f6a75a4a54b
GhadiFalouh <ghadif2001@hotmail.com>
2023-05-18 22:50:49 -0400

    forms

--- members/forms.py
+++ members/forms.py
@@ -5,6 +5,10 @@ from django.forms import ModelForm
 
 
 class CreateUserForm(UserCreationForm):
+    """
+    La form de l'utilisateur qui possède un username, un email, un mot de passe et un nom
+    cela détermine à quoi ressemble un user
+    """
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
